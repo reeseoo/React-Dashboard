07fb041771baf7d618b096762e81b23d02f1c4ba
reeseoo <reesejmorris@gmail.com>
2016-11-02 13:20:36 +0000

    One last test of Git

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ var ReactDOM = require('react-dom');
 var HelloWorld = React.createClass({
     render: function(){
         return (
-            <div>Another test</div>
+            <div>One last test</div>
         )
     }
 });
