e1b30a4182c32bd94fb81c8891f1cdacd83be238
reeseoo <reesejmorris@gmail.com>
2016-11-02 14:14:14 +0000

    Added some input and added folders for components

--- app/index.js
+++ app/index.js
@@ -2,14 +2,22 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var HelloWorld = React.createClass({
+
     render: function(){
         return (
-            <div>One last test</div>
+            <div>
+                <select>
+                    <option value="A">Apple</option>
+                    <option value="B">Banana</option>
+                    <option value="C">Cranberry</option>
+                </select>
+                One last test {this.props.name}
+            </div>
         )
     }
 });
 
 ReactDOM.render(
-    <HelloWorld />,
+    <HelloWorld name="Reese"/>,
     document.getElementById('app')
 )
