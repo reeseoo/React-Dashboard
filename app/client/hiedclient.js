0c1f3fc6a1fe855adcef5760f0b620582cb278af
reeseoo <reesejmorris@gmail.com>
2016-11-14 22:15:07 +0000

    Implementing fetching from API.

---
+++ app/client/hiedclient.js
@@ -0,0 +1,23 @@
+export class HiedClient {
+    getCounts() {
+        var url = "http://hi/apps/HiTicketsApi/api/getrequestuser/7151";
+
+        fetch(url)
+            .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                    return;
+                }
+                
+                response.json().then(function (data) {
+                    console.log(data);
+                });
+            }
+            )
+            .catch(function (err) {
+                console.log('Fetch Error :-S', err);
+            });
+    }
+}
--- app/components/Main.js
+++ app/components/Main.js
@@ -1,4 +1,5 @@
 var React = require('React');
+var HiedClient = require('HiedClient')
 
 var Main = React.createClass({
     render: function () {
