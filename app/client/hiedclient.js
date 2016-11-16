9adb158bbd2278feec3b127067e35b3bc42ba4e4
reeseoo <reesejmorris@gmail.com>
2016-11-16 12:53:36 +0000

    Hooked up charts to client data.

--- app/client/hiedclient.js
+++ app/client/hiedclient.js
@@ -2,26 +2,7 @@ class HiedClient {
     getCounts(userId) {
         var url = "http://hi/apps/HiTicketsApi/api/count/getmycounts/";
 
-        fetch(url + userId)
-            .then(
-            function (response) {
-                var returnData = 0;
-                if (response.status !== 200) {
-                    console.log('Looks like there was a problem. Status Code: ' +
-                        response.status);
-                    return 0;
-                }
-
-                response.json().then(function (data) {
-                    console.log(data);
-                    returnData = data;
-                    console.log("returnData = " + returnData)
-                    return returnData;
-                });
-            })
-            .catch(function (err) {
-                console.log('Fetch Error :-S', err);
-            });
+        return fetch(url + userId);
     }
 }
 
--- app/components/Home.js
+++ app/components/Home.js
@@ -3,79 +3,130 @@ import Doughnut from 'react-chartjs-2';
 import HiedClient from '../client/hiedClient.js';
 
 class Home extends React.Component {
-
-    //new HiedClient().getCounts(7151)
-    //new HiedClient().getCounts(7144)
-    //new HiedClient().getCounts(1116)
-    //new HiedClient().getCounts(7158)
-
     constructor() {
         super();
         this.client = new HiedClient();
-        console.log("From home " + this.client.getCounts(7151));
+        this.apiReposnse = 0;
+        this.chartData;
+        this.userOneCounts = 0;
+        this.userTwoCounts = 0;
+        this.userThreeCounts = 0;
+        this.userFourCounts = 0;
+        this.state = {
+            isLoading: true
+        };
     }
 
-    // chartData() {
-    //     return new {
-    //         labels: ["Reese", "Damien", "Chris", "James"],
-    //         datasets: [{
-    //             label: '# of Votes',
-    //             data: [5, 10, 7, 15],
-    //             backgroundColor: [
-    //                 'rgba(255, 99, 132, 0.2)',
-    //                 'rgba(54, 162, 235, 0.2)',
-    //                 'rgba(255, 206, 86, 0.2)',
-    //                 'rgba(75, 192, 192, 0.2)'
-    //             ],
-    //             borderColor: [
-    //                 'rgba(255,99,132,1)',
-    //                 'rgba(54, 162, 235, 1)',
-    //                 'rgba(255, 206, 86, 1)',
-    //                 'rgba(75, 192, 192, 1)'
-    //             ],
-    //             borderWidth: 1
-    //         }]
-    //     }
-    // }
+    start() {
+        if (this.state.isLoading) {
+            this.getChartData();
+            this.setAllCounts();
 
-    // chartOptions() {
-    //     return {
-    //         scales: {
-    //             yAxes: [{
-    //                 ticks: {
-    //                     beginAtZero: true
-    //                 }
-    //             }]
-    //         }
-    //     }
-    // }
+            return (
+                <p>Loading Data</p>
+            )
+        } else {
+            debugger;
+            return (
+                <Doughnut data={this.chartData} />
+            )
+        }
+    }
+
+    setUserOneCounts() {
+        var self = this;
+        this.client.getCounts(7151).then(res => {
+            res.json().then(function (data) {
+                console.log("userdata = " + data)
+                self.chartData.datasets[0].data.push(data);
+                self.chartData.labels.push('Reese');
+                self.setState({
+                    isLoading: false
+                });
+            });
+        });
+    }
+
+    setUserTwoCounts() {
+        var self = this;
+        this.client.getCounts(7144).then(res => {
+             res.json().then(function (data) {
+                console.log("userdata = " + data)
+                self.chartData.datasets[0].data.push(data);
+                self.chartData.labels.push('Damien');
+                self.setState({
+                    isLoading: false
+                });
+            });
+        });
+    }
+
+    setUserThreeCounts() {
+        var self = this;
+        this.client.getCounts(1116).then(res => {
+            res.json().then(function (data) {
+                console.log("userdata = " + data)
+                self.chartData.datasets[0].data.push(data);
+                self.chartData.labels.push('Chris');
+                self.setState({
+                    isLoading: false
+                });
+            });
+        });
+    }
+
+    setUserFourCounts() {
+        var self = this;
+        this.client.getCounts(7158).then(res => {
+            debugger;
+            res.json().then(function (data) {
+                console.log("userdata = " + data)
+                self.chartData.datasets[0].data.push(data);
+                self.chartData.labels.push('James');
+                self.setState({
+                    isLoading: false
+                });
+            });
+        });
+    }
+
+    setAllCounts() {
+        this.userOneCounts = this.setUserOneCounts();
+        this.userTwoCounts = this.setUserTwoCounts();
+        this.userThreeCounts = this.setUserThreeCounts();
+        this.userFourCounts = this.setUserFourCounts();
+    }
+
+    getChartData() {
+        this.chartData = {
+            labels: [],
+            datasets: [{
+                label: 'Active Tickets',
+                data: [],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)'
+                ],
+                borderWidth: 1
+            }]
+        }
+    }
 
     render() {
         return (
-            <Doughnut data={chartData} />
+            <div>
+                {this.start()}
+            </div>
         )
     }
 };
 
 export default Home;
-
-var chartData = {
-    labels: ["Reese", "Damien", "Chris", "James"],
-    datasets: [{
-        label: '# of Votes',
-        data: [5, 10, 7, 15],
-        backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)'
-        ],
-        borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)'
-        ],
-        borderWidth: 1
-    }]
-}
